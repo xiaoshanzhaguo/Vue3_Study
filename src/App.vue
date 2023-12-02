d2e58c4093f5a8a25c02e33096d731dd5fb33c76
shanzhaguo <xiaoshanzhaguo@163.com>
2023-12-02 20:45:46 +0800

    feat: watchEffect高级侦听器

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,33 @@
 <template>
-  <div>
-    case1: <input v-model="message" type="text">
-    <hr>
-    case2: <input v-model="message2" type="text">
-    <hr>
-    case3: <input v-model="message3.foo.bar.name" type="text">
-    <hr>
-    case4: <input v-model="message3.foo.bar.age" type="text">
-  </div>
+  <input id="ipt" v-model="message" type="text">
+  <input v-model="message2" type="text">
+  <button @click="stopWatch">停止监听</button>
 </template>
 <script setup lang='ts'>
-import { ref, reactive, watch } from 'vue';
+import { ref, reactive, watch, watchEffect } from 'vue';
 let message = ref<string>('小美')
 let message2 = ref<string>('大美')
 
-let message3 = ref({
-  foo: {
-    bar: {
-      name: "小美",
-      age: 18
-    }
+
+const stop = watchEffect((onInvalidate) => {
+  let ipt: HTMLInputElement = document.querySelector('#ipt') as HTMLInputElement
+  console.log(ipt, 'e111111111');
+
+  console.log('message====>', message.value);
+  // 需要监听多个的话，要再添加一个log
+  console.log('message====>', message2.value);
+  // watchEffect在监测到依赖时,会先执行他的形参onInvalidate函数,再执行作用域的函数
+  onInvalidate(() =>  {
+    console.log('before111');
+  })
+}, {
+  flush: "post",
+  onTrigger(e) {
+    debugger
   }
 })
 
-watch([message, message2], (newVal, oldVal) => {
-  console.log(newVal, oldVal);
-})
-
-
-watch(message3, (newVal, oldVal) => {
-  console.log(newVal, oldVal);
-}, {
-  deep: true  // 深度监听
-})
-
-watch(() => message3.value.foo.bar.age, (newVal, oldVal) => {
-  console.log(newVal, oldVal);
-}, {
-  // deep: true  // 深度监听
-  immediate: true, // 默认为false。如果为true，代表立即执行一次
-  flush: "pre" // 默认为pre 组件更新前调用，sync 同步执行，post 组件更新后调用
-})
+const stopWatch = () => stop()
 </script>
 <style scoped>
 
