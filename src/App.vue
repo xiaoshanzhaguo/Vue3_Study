4de326d69e50e844a84abda3ba176ba4b87aaef0
shanzhaguo <xiaoshanzhaguo@163.com>
2023-12-25 22:02:42 +0800

    feat: 生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
-  <input id="ipt" v-model="message" type="text">
-  <input v-model="message2" type="text">
-  <button @click="stopWatch">停止监听</button>
+  <h1>生命周期</h1>
+  <A v-if="flag"></A>
+  <button @click="flag = !flag">创建-销毁</button>
 </template>
+
 <script setup lang='ts'>
-import { ref, reactive, watch, watchEffect } from 'vue';
-let message = ref<string>('小美')
-let message2 = ref<string>('大美')
-
-
-const stop = watchEffect((onInvalidate) => {
-  let ipt: HTMLInputElement = document.querySelector('#ipt') as HTMLInputElement
-  console.log(ipt, 'e111111111');
-
-  console.log('message====>', message.value);
-  // 需要监听多个的话，要再添加一个log
-  console.log('message====>', message2.value);
-  // watchEffect在监测到依赖时,会先执行他的形参onInvalidate函数,再执行作用域的函数
-  onInvalidate(() =>  {
-    console.log('before111');
-  })
-}, {
-  flush: "post",
-  onTrigger(e) {
-    debugger
-  }
-})
-
-const stopWatch = () => stop()
+import {ref, reactive} from 'vue'
+import A from './components/A.vue'
+const flag = ref<Boolean>(true)
 </script>
+
 <style scoped>
 
 </style>
