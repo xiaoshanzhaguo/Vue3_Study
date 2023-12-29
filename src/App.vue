238500feb49f933224dcd0d598578f28bccd2ac3
shanzhaguo <xiaoshanzhaguo@163.com>
2023-12-29 19:48:21 +0800

    feat: 子组件向父组件传值

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>父级</h1>
-  <WaterFall :arr="[1, 2, 3]" :title="name"></WaterFall>
+  <WaterFall @on-click="getName" :arr="[1, 2, 3]" :title="name"></WaterFall>
 </template>
 
 <script setup lang='ts'>
 import {ref, reactive} from 'vue'
 import WaterFall from './components/WaterFall.vue'
 const name = ref<string>('小宝')
+
+
+const getName = (name: string) => {
+  console.log(name, '=====>我是父组件')
+}
 </script>
 
 <style scoped>
--- src/components/WaterFall.vue
+++ src/components/WaterFall.vue
@@ -1,32 +1,20 @@
 <template>
  <hr />
  <div>子集</div>
- <div>值：{{ title }}</div>
- <div>值：{{ arr }}</div>
+ <button @click="send">子组件向父组件传值</button>
 </template>
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-// 1. js写法
-// 接收父组件传过来的值defineProps
-// const props = defineProps({
-//     title: {
-//         type: String,   // 注意这里的String是放的基本数据类型的值，而不是string
-//         default: "默认值"
-//     }
-// })
+// 给父组件传值 defineEmits js写法
+// const emit = defineEmits(['on-click'])
+// ts写法
+const emit = defineEmits<{
+    (e: "on-click", name: string): void
+}>()
 
-// 2. ts写法
-// defineProps({
-//     title: String,
-//     arr: Array
-// })
-// 2.1 ts 特有定义默认值(withDefaults)  使用泛型的写法
-withDefaults(defineProps<{
-    title: string,
-    arr: number[]
-}>(), {
-    arr: () => [666]
-})
+const send = () => {
+    emit('on-click', '小美')
+}
 </script>
 <style scoped>
 
