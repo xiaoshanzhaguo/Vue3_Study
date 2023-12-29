ce65d4b0ab9860897d1bd0476fa2832dce739158
shanzhaguo <xiaoshanzhaguo@163.com>
2023-12-29 19:37:27 +0800

    父组件向子组件传值

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <h1>生命周期</h1>
-  <A v-if="flag"></A>
-  <button @click="flag = !flag">创建-销毁</button>
+  <h1>父级</h1>
+  <WaterFall :arr="[1, 2, 3]" :title="name"></WaterFall>
 </template>
 
 <script setup lang='ts'>
 import {ref, reactive} from 'vue'
-import A from './components/A.vue'
-const flag = ref<Boolean>(true)
+import WaterFall from './components/WaterFall.vue'
+const name = ref<string>('小宝')
 </script>
 
 <style scoped>
---
+++ src/components/WaterFall.vue
@@ -0,0 +1,33 @@
+<template>
+ <hr />
+ <div>子集</div>
+ <div>值：{{ title }}</div>
+ <div>值：{{ arr }}</div>
+</template>
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+// 1. js写法
+// 接收父组件传过来的值defineProps
+// const props = defineProps({
+//     title: {
+//         type: String,   // 注意这里的String是放的基本数据类型的值，而不是string
+//         default: "默认值"
+//     }
+// })
+
+// 2. ts写法
+// defineProps({
+//     title: String,
+//     arr: Array
+// })
+// 2.1 ts 特有定义默认值(withDefaults)  使用泛型的写法
+withDefaults(defineProps<{
+    title: string,
+    arr: number[]
+}>(), {
+    arr: () => [666]
+})
+</script>
+<style scoped>
+
+</style>
