ba366cc9e990c63fe195f21fa9d96c70672af5a7
shanzhaguo <xiaoshanzhaguo@163.com>
2024-04-15 16:32:53 +0800

    feat: transitionGroup

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -1,112 +1,40 @@
 <template>
  <div class="content">
-    <button @click="flag = !flag">switch</button>
-    <transition
-        appear
-        appear-from-class="from"
-        appear-active-class="active"
-        appear-to-class="to"
-    >
-        <div v-if="flag" class="box"></div>
-    </transition>
+    <button @click="add">ADD</button> <button @click="pop">POP</button>
+    <div class="wraps">
+        <transition-group 
+            leave-active-class="animate__animated animate__bounceOut" 
+            enter-active-class="animate__animated animate__bounceIn">
+            <div class="item" :key="item" v-for="item in list">{{ item }}</div>
+        </transition-group>
+    </div>
  </div>
 </template>
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-import gsap from 'gsap'
-const flag = ref<boolean>(true)
-const EnterFrom = (el: Element) => {
-    // console.log('进入之前');
-    gsap.set(el, {
-        width: 0,
-        height: 0
-    })
+import 'animate.css'
+
+const list = reactive<number[]>([1,2,3,4,5,6])
+
+const add = () => {
+    list.push(list.length + 1);
 }
-const EnterActive = (el: Element, done: gsap.Callback) => {
-    // console.log('过渡曲线');
-    // setTimeout(() => {
-    //     done();
-    // }, 3000)
-    gsap.to(el, {
-        width: 200,
-        height: 200,
-        onComplete: done
-    })
+
+const pop = () => {
+    list.pop();
 }
-const EnterTo = (el: Element) => {
-    console.log('过渡完成');
-}
-const EnterCancelled = (el: Element) => {
-    console.log('过渡效果被打断');
-}
-const LeaveFrom = (el: Element) => {
-    console.log('离开之前');
-}
-const Leave = (el: Element, done: gsap.Callback) => {
-    // console.log('离开过渡曲线');
-    // setTimeout(() => {
-    //     done();
-    // }, 3000)
-    gsap.to(el, {
-        width: 0,
-        height: 0,
-        onComplete: done
-    })
-}
-const LeaveTo = (el: Element) => {
-    console.log('离开完成');
-}
-const LeaveCancelled = (el: Element) => {
-    console.log('离开过渡曲线被打断----------');
-}
+
 </script>
 <style lang="less" scoped>
-.box {
-    width: 200px;
-    height: 200px;
-    background: red;
-}
-.from {
-    width: 0;
-    height: 0;
-}
-.active {
-    transition: all 2s ease;
-}
-.to {
-    width: 200px;
-    height: 200px;
-}
-/* // 开始过渡 */
-.fade-enter-from{
-   background:red;
-   width:0px;
-   height:0px;
-   transform:rotate(360deg)
-}
-/* // 开始过渡了 */
-.fade-enter-active{
-  transition: all 2.5s linear;
-}
-/* // 过渡完成 */
-.fade-enter-to{
-   background:yellow;
-   width:200px;
-   height:200px;
-}
-/* // 离开的过渡 */
-.fade-leave-from{
-  width:200px;
-  height:200px;
-  transform:rotate(360deg)
-}
-/* // 离开中过渡 */
-.fade-leave-active{
-  transition: all 1s linear;
-}
-/* // 离开完成 */
-.fade-leave-to{
-  width:0px;
-  height:0px;
+.wraps {
+    display: flex;
+    flex-wrap: wrap;
+    word-break: break-all;
+    border: 1px solid #ccc;
+
+    .item {
+        margin: 10px;
+        font-size: 20px;
+    }
 }
 </style>
