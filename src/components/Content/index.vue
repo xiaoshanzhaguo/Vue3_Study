07a37627e4ab8fa2c7a627c7af1059b1967dab5a
shanzhaguo <xiaoshanzhaguo@163.com>
2024-04-15 16:59:05 +0800

    feat: 状态过渡

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -1,54 +1,30 @@
 <template>
  <div>
-    <button @click="random">random</button>
-    <!-- move-class为平移的动画 -->
-    <transition-group move-class="mmm" class="wraps" tag="div">
-        <div class="items" :key="item.id" v-for="item in list">
-            {{ item.number }}
-        </div>
-    </transition-group>
+   <input v-model="num.current" step="20" type="number">
+   <div>
+    {{ num.tweenedNumber.toFixed(0) }}
+   </div>
  </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue'
-import _ from 'lodash' // lodash报错，需要安装ts声明文件库 npm install @types/lodash -D
+import { reactive, watch } from 'vue'
+import gsap from 'gsap';
 
-// 由于ts约束，这里断言成number数组
-let list = ref(Array.apply(null, {length: 81} as number[]).map((_, index) => {
-    return {
-        id: index,
-        number: (index % 9) + 1
-    }
-}))
+const num = reactive({
+    current: 0,
+    tweenedNumber: 0
+})
 
-console.log(list.value); // 这里就是一个对象数组
-
-// 随机用lodash库
-const random = () => {
-    list.value = _.shuffle(list.value)
-}
+watch(() => num.current, (newVal, oldVal) => {
+    gsap.to(num, {
+        duration: 1,
+        tweenedNumber: newVal
+    })
+})
 
 </script>
 
 <style lang="less" scoped>
-.wraps {
-    display: flex;
-    flex-wrap: wrap;
-    width: calc(25px * 10 + 9px);
 
-    .items {
-        width: 25px;
-        height: 25px;
-        border: 1px solid #ccd;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-}
-
-// transition是通过transform和translate来实现的，这里可以加all，或者不加
-.mmm {
-    transition: all 1s;
-}
 </style>
