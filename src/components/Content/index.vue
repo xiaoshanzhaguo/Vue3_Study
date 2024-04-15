a48018b414884a67d726a74ebb6227ecdbd18a2c
shanzhaguo <xiaoshanzhaguo@163.com>
2024-04-15 16:49:48 +0800

    feat: 列表的移动过渡

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -1,40 +1,54 @@
 <template>
- <div class="content">
-    <button @click="add">ADD</button> <button @click="pop">POP</button>
-    <div class="wraps">
-        <transition-group 
-            leave-active-class="animate__animated animate__bounceOut" 
-            enter-active-class="animate__animated animate__bounceIn">
-            <div class="item" :key="item" v-for="item in list">{{ item }}</div>
-        </transition-group>
-    </div>
+ <div>
+    <button @click="random">random</button>
+    <!-- move-class为平移的动画 -->
+    <transition-group move-class="mmm" class="wraps" tag="div">
+        <div class="items" :key="item.id" v-for="item in list">
+            {{ item.number }}
+        </div>
+    </transition-group>
  </div>
 </template>
+
 <script setup lang='ts'>
-import { ref, reactive } from 'vue'
-import 'animate.css'
+import { ref } from 'vue'
+import _ from 'lodash' // lodash报错，需要安装ts声明文件库 npm install @types/lodash -D
 
-const list = reactive<number[]>([1,2,3,4,5,6])
+// 由于ts约束，这里断言成number数组
+let list = ref(Array.apply(null, {length: 81} as number[]).map((_, index) => {
+    return {
+        id: index,
+        number: (index % 9) + 1
+    }
+}))
 
-const add = () => {
-    list.push(list.length + 1);
-}
+console.log(list.value); // 这里就是一个对象数组
 
-const pop = () => {
-    list.pop();
+// 随机用lodash库
+const random = () => {
+    list.value = _.shuffle(list.value)
 }
 
 </script>
+
 <style lang="less" scoped>
 .wraps {
     display: flex;
     flex-wrap: wrap;
-    word-break: break-all;
-    border: 1px solid #ccc;
+    width: calc(25px * 10 + 9px);
 
-    .item {
-        margin: 10px;
-        font-size: 20px;
+    .items {
+        width: 25px;
+        height: 25px;
+        border: 1px solid #ccd;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
+}
+
+// transition是通过transform和translate来实现的，这里可以加all，或者不加
+.mmm {
+    transition: all 1s;
 }
 </style>
