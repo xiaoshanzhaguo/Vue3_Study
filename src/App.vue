bf992fc06b8bb27e2c1e35e37b2095471054b3d3
shanzhaguo <xiaoshanzhaguo@163.com>
2024-04-16 15:50:19 +0800

    feat: Vue3自动引入插件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <div>
-    <xiaomei @on-click="getItem" name="小美props"></xiaomei>
-  </div>
+  <button @click="flag = !flag">change flag</button>
+  <div>{{ flag }}</div>
  </template>
  
 <script setup lang='ts'>
-import { ref, reactive } from 'vue'
-// 注意，引入的时候不用加后缀，它会自己进行识别
-import xiaomei from './App'
-
-const getItem = (item: any) => {
-  console.log('父组件收到了', item)
-}
+let flag = ref<boolean>(false)
 </script>
  
 <style lang="less" scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import DefineOptions from 'unplugin-vue-define-options/vite'
 import vue from '@vitejs/plugin-vue'
 // import vueJsx from '@vitejs/plugin-vue-jsx'
 import tsx from './plugin/index'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,10 @@ export default defineConfig({
     vue(),
     DefineOptions(),
     // vueJsx(),
-    tsx()
+    tsx(),
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-import.d.ts' // 帮我们生成声明文件，不然直接使用找不到
+    })
   ]
 })
