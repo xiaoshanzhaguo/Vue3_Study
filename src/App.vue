4015140cbb446ff56cbc72213cfba695d9d15542
shanzhaguo <xiaoshanzhaguo@163.com>
2024-04-18 15:42:56 +0800

    feat: 图片懒加载

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
 <template>
-  <div v-move class="box">
-    <div class="header"></div>
-    <div>内容</div>
-  </div>
- </template>
- 
-<script setup lang='ts'>
-import { ref, Directive, DirectiveBinding} from 'vue';
 
-const vMove:Directive<any, void> = (el: HTMLElement, bingding: DirectiveBinding) => {
-  let moveElement:HTMLDivElement = el.firstElementChild as HTMLDivElement
-  console.log(moveElement);
-  const mouseDown = (e: MouseEvent) => {
-    let X = e.clientX - el.offsetLeft;
-    let Y = e.clientY - el.offsetTop;
-    const move = (e: MouseEvent) => {
-      console.log(e);
-      el.style.left = e.clientX - X + 'px';
-      el.style.top = e.clientY - Y + 'px';
+ <div>
+  <div>
+    <img v-lazy="item" width="360" height="500" v-for="item in arr" alt="">
+  </div>
+ </div>
+
+</template>
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+import type { Directive } from 'vue';
+// glob 是懒加载的模式
+// let modules = {
+//   'xxxxx': () => import('xxxxx') // 遇到import会进行代码分包
+// }
+// globEager 静态加载
+// import xxx from 'xxx'
+
+let imageList: Record<string, {default: string}> = import.meta.glob('./assets/images/*.*', {eager: true})
+let arr = Object.values(imageList).map(v => v.default)
+
+let vLazy:Directive<HTMLImageElement, string> = async (el, bingding) => {
+  const def = await import('./assets/vue.svg')
+  el.src = def.default
+  const observer = new IntersectionObserver((enr) => {
+    console.log(enr[0], bingding.value);
+    if (enr[0].intersectionRatio > 0) {
+      el.src = bingding.value
+      observer.unobserve(el)
     }
-    document.addEventListener('mousemove', move)
-    document.addEventListener('mouseup', () => {
-      document.removeEventListener('mousemove', move)
-    })
-  }
-  moveElement.addEventListener('mousedown', mouseDown)
+  })
+  observer.observe(el);
 }
 </script>
- 
-<style lang="less" scoped>
+<style scoped>
 
-.box {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 200px;
-  height: 200px;
-  border: 1px solid #ccc;
-  .header {
-    height: 20px;
-    background: black;
-    cursor: move;
-  }
-}
 </style>
